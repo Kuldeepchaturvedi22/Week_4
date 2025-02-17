044c9dcdf143f8b45699496bac15a7d1eda6e28d
Kuldeepchaturvedi22 <chaturvedikuldeep40@gmail.com>
2025-02-17 10:43:02 +0530

    [Kuldeep] Add. Added program of Use @Override Correctly

---
+++ day_6/src/main/com/Annotations/Exercise/Main.java
@@ -0,0 +1,25 @@
+package com.Annotations.Exercise;
+
+class Animal {
+    // Method to make a sound
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    // Override the makeSound method to provide specific implementation for Dog
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of Dog
+        Dog dog = new Dog();
+        // Call the overridden makeSound method
+        dog.makeSound();
+    }
+}
---
+++ day_6/src/main/com/Annotations/IntermediateLevel/Main.java
@@ -0,0 +1,4 @@
+package com.Annotations.IntermediateLevel;
+
+public class Main {
+}
